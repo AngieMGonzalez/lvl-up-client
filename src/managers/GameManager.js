8ce57e890240e0de1d47d2120d15fbdca4fe37bc
Angie Gonzalez <angiem.go@gmail.com>
2023-05-09 09:41:18 -0500

    bare bones games and events links to lists

--- src/managers/GameManager.js
+++ src/managers/GameManager.js
@@ -1,10 +1,10 @@
 // the Authorization header will be in every fetch call to the database to let the server know which user is logged in
 
 export const getGames = () => {
-  return fetch("http://localhost:8000/games", {
-      headers:{
-          "Authorization": `Token ${localStorage.getItem("lu_token")}`
-      }
-  })
-      .then(response => response.json())
+    return fetch("http://localhost:8000/games", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
 }
